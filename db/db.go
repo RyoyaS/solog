5843ec305d958e03f0584f767a3120d76dbe10c5
ryoya <ryouya1998.1.21@gmail.com>
2021-05-10 22:56:35 +0900

    update index

--- db/db.go
+++ db/db.go
@@ -17,25 +17,25 @@ type User struct {
 func DbInit() {
 	db, err := gorm.Open("sqlite3", "solog.sqlite3")
 	if err != nil {
-		panic("データベースが開けません(dbInit)")
+		panic("can not open database(dbInit)")
 	}
 	db.AutoMigrate(&User{})
 	defer db.Close()
 }
 
-func DbInsert(name string, email string, password string, sex int, birthday string) {
+func DbUsertInsert(name string, email string, password string, sex int, birthday string) {
 	db, err := gorm.Open("sqlite3", "solog.sqlite3")
 	if err != nil {
-		panic("データベース開けず！（dbInsert)")
+		panic("can not open database(dbInsert)")
 	}
 	db.Create(&User{Name: name, Email: email, Password: password, Sex: sex, Birthday: birthday})
 	defer db.Close()
 }
 
-func DbGetOne(email string) User {
+func DbUserGetOne(email string) User {
 	db, err := gorm.Open("sqlite3", "solog.sqlite3")
 	if err != nil {
-		panic("データベース開けず！(dbGetOne())")
+		panic("can not open database(dbGetOne())")
 	}
 	var user User
 	db.First(&user, email)
